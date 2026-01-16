87351ed934e33271289daa69c37c72194de9fc14
aslam-16 <slamaslam466@gmail.com>
2026-01-16 18:24:38 +0530

    testing routes

--- src/server.js
+++ src/server.js
@@ -2,8 +2,33 @@ const express=require('express');
 const app=express();
 const port=1616;
 
-app.use('/test',(req,res)=>{
-    res.send('Hello from DevNode server!');
+
+// app.use('/test',(req,res)=>{
+//     res.send('Hello from DevNode server!');
+// })
+
+// app.get(/ab+c/,(req,res)=>{
+//     res.send(`from get /route ${req.url}`);
+// });
+app.get('/',(req,res)=>{
+    res.send(`from get route ${req.url}`);
+})
+
+app.get('/user',(req,res)=>{
+    console.log(req.query);
+    
+    res.send(`from get route ${req.url}`);
+})
+
+app.post('/',(req,res)=>{
+    let user={
+        name:'aslam'
+    }
+    res.send(user);
+})
+//always keep it in the end due to its hierarchy issue and order of routes matters a lot
+app.use('/',(req,res,next)=>{
+    res.send('from / route');
 })
 
 app.listen(port,()=>{
