7789354aecdb275caf2db1344e1ef6a95390f1de
aslam-16 <slamaslam466@gmail.com>
2026-01-16 19:36:22 +0530

    multiple routes handler with next

--- src/server.js
+++ src/server.js
@@ -10,26 +10,20 @@ const port=1616;
 // app.get(/ab+c/,(req,res)=>{
 //     res.send(`from get /route ${req.url}`);
 // });
-app.get('/',(req,res)=>{
-    res.send(`from get route ${req.url}`);
-})
-
-app.get('/user',(req,res)=>{
-    console.log(req.query);
-    
-    res.send(`from get route ${req.url}`);
-})
-
-app.post('/',(req,res)=>{
-    let user={
-        name:'aslam'
-    }
-    res.send(user);
-})
-//always keep it in the end due to its hierarchy issue and order of routes matters a lot
-app.use('/',(req,res,next)=>{
-    res.send('from / route');
-})
+app.get('/',(req,res,next)=>{
+    next();
+    console.log('This is the first middleware.');
+},
+(req,res,next)=>{
+    next();
+    console.log('This is the second middleware.');
+        
+},
+(req,res,next)=>{
+    console.log('This is the third middleware.');
+    //next();
+    res.send('hi there, welcome to DevNode server!');
+});
 
 app.listen(port,()=>{
     console.log(`DevNode server is running at http://localhost:${port}`);
