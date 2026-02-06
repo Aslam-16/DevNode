702054be94447cfb9905d776aa95b1deb5fa80c6
aslam-16 <slamaslam466@gmail.com>
2026-02-06 12:59:16 +0530

    put vs patch and delete

---
+++ readme.md
@@ -0,0 +1,5 @@
+🟢 Learned about the mongoose connect thru compass and made it as a module to use it at     server.
+🟢 worked on get  and post method in which to perform the post we need to set up the app to use express.json.
+🟢  Learned about the middleware which is next() in each route or api handler, which validates the routes thru authorization and authentication and passes to next route, if it is a valid user. We can also pass it as a second parameter in the route itself with a explicit function to check individual routes for modular and efficient check.
+🟢 Started working on the delete and update methods.
+🟢🟢🟢
--- src/server.js
+++ src/server.js
@@ -4,6 +4,10 @@ const port=1616;
 const connectDB=require('./config/db');
 const {adminauth,userauth}=require('./middlewares/auth');
 const User = require('./model/users');
+//to parse json data in request body and allow any post request 
+// to accept the incoming req body
+
+app.use(express.json());
 
 app.use('/',(err,req,res,next)=>{
     console.error(err.stack);
@@ -22,21 +26,65 @@ app.get('/user/home',userauth,(req,res)=>{
 });
 
 app.post('/signup',async(req,res)=>{
-    const userdata={
-        firstName:"Mohamed",
-        lastName:"Aslam",
-        email:"slamaslam@gmail.com",
-        password:"12345"
-    }
-    try{const user=new User(userdata);
+    
+
+    try{const user=new User(req.body);
     await user.save();
     res.send('user registered successfully');}
     catch(err){
         res.status(500).send('error in user registration');
     }
+    console.log(req.body)
 
 });
 
+app.get('/listusers',async(req,res)=>{
+    try{
+        const users=await User.find();
+        res.send(users);
+    }
+    catch(err){
+        res.status(500).send('error in fetching users');
+    }   
+});
+
+app.delete('/deleteuser',async(req,res)=>{
+    try{
+        const {id}=req.body;
+        const user=await User.findByIdAndDelete(id,returnDocument='after');
+        console.log("kk",user);
+        res.send('user deleted successfully');
+    }
+    catch(err){
+        res.status(500).send('error in deleting user');
+    }
+});
+//patch method will update only the fields provided in the request body and keep the other fields unchanged
+app.patch('/updateuser',async(req,res)=>{
+    try{
+        const {id,...updateData}=req.body;
+        const user=await User.findByIdAndUpdate(id,updateData,{returnDocument:'after'});
+        console.log("kk",user);
+        res.send('user updated successfully');
+    }
+    catch(err){
+        res.status(500).send('error in updating user');
+    }
+});
+//put method will replace the entire document with the new data provided in the request body
+app.put('/updateuser',async(req,res)=>{
+    try{
+        const {id,...updateData}=req.body;
+        const user=await User.findOneAndReplace({_id:id},updateData,{returnDocument:'after'});
+        console.log("kk",user);
+        res.send('put user updated successfully');
+    }
+    catch(err){
+        console.log(err);
+        
+        res.status(500).send('error in updating user');
+    }
+});
 
 connectDB.then(()=>{
     console.log('database connected successfully');
